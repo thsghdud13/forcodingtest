ee4141e9b00854557a4bf4148318b024cfc54df1
thsghdud13 <thsgdud25@naver.com>
2022-08-02 21:26:43 +0900

    boj14501

--- dynamic_programing/boj14501.cpp
+++ dynamic_programing/boj14501.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n;
+int d[20];
+int t[20];
+int p[20];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cin>>n;
+    for(int i = 1; i <= n; i++){
+        cin>>t[i]>>p[i];
+    }
+    for(int i = n; i >= 1; i--){
+        if(i + t[i] <= n + 1){
+            d[i] = max(d[i + t[i]] + p[i] ,d[i + 1]);
+        }
+        else{
+            d[i] = d[i + 1];
+        }
+    }
+    cout<<*max_element(d + 1, d + n + 1);
 }
