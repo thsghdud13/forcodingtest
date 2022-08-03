b64986624702d67c2413b655896464aeb57b3e76
thsghdud13 <thsgdud25@naver.com>
2022-08-03 20:52:48 +0900

    boj2748

---
+++ dynamic_programing/boj2748.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long d[100];
+int n;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    d[0] = 0;
+    d[1] = 1;
+
+    for(int i = 2; i <= n; i++){
+        d[i] = d[i-2] + d[i-1];
+    }
+    cout<<d[n];
+}
